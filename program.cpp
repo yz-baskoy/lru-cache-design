bb5ff601b069401637f5e92d442fcd082320f020
yz-baskoy <yziyabaskoy@gmail.com>
2023-07-16 04:33:15 +0300

    Update readme

--- program.cpp
+++ program.cpp
@@ -5,62 +5,61 @@
 
 using namespace std;
 
-class Person 
-{
-public:
-	string idNumber;
-	string fullName;
-	string birthDate;
-	string adress;
+class Person {
+	public:
+		string idNumber;
+		string fullName;
+		string birthDate;
+		string adress;
 
-Person(string idNumber, string fullName, string birthDate, string adress) {
-	this->idNumber = idNumber;
-	this->fullName = fullName;
-	this->adress = adress;
-	this->birthDate = birthDate;
+	Person(string idNumber, string fullName, string birthDate, string adress) {
+		this->idNumber = idNumber;
+		this->fullName = fullName;
+		this->adress = adress;
+		this->birthDate = birthDate;
 }
 };
 
-class LruCache 
-{
-private:
-	int capacity;
-	double loadFactor;
-	list<Person> cacheList;
-	unordered_map<string, list<Person>::iterator> cacheMap;
-public: 
-	LruCache(int capacity, double loadFactor) {
-		this->capacity = capacity;
-		this->loadFactor = loadFactor;
-	}
-
-	Person* getPerson(string idNumber) {
-		if (cacheMap.find(idNumber) == cacheMap.end())
-		{
-			return NULL;
+class LruCache {
+	private:
+		int capacity;
+		double loadFactor;
+		list<Person> cacheList;
+		unordered_map<string, list<Person>::iterator> cacheMap;
+	public: 
+		LruCache(int capacity, double loadFactor) {
+			this->capacity = capacity;
+			this->loadFactor = loadFactor;
 		}
-		list<Person>::iterator itr = cacheMap[idNumber];
-		Person person = *itr;
-		std::cout<< "person aq" << person.fullName << endl;
-		cacheList.erase(itr);
-		cacheList.push_front(person);
-		cacheMap[idNumber] = cacheList.begin();
 
-		return &cacheList.front();
-	}
-    // Yeni bir kişiyi cache'e ekleme
-    void addPerson(Person person) {
-        // Cache dolu ise en son kullanılan kişiyi sil
-        if (cacheList.size() >= capacity) {
-            Person lastPerson = cacheList.back();
-            cacheList.pop_back();
-            cacheMap.erase(lastPerson.idNumber);
-        }
+		Person* getPerson(string idNumber) {
+			if (cacheMap.find(idNumber) == cacheMap.end())
+			{
+				return NULL;
+			}
+			list<Person>::iterator itr = cacheMap[idNumber];
+			Person person = *itr;
+			std::cout<< "person aq" << person.fullName << endl;
+			std::cout<< &itr << endl;
+			cacheList.erase(itr);
+			cacheList.push_front(person);
+			cacheMap[idNumber] = cacheList.begin();
 
-        // Yeni kişiyi cache'in başına ekle
-        cacheList.push_front(person);
-        cacheMap[person.idNumber] = cacheList.begin();
-    }	
+			return &cacheList.front();
+		}
+		// Yeni bir kişiyi cache'e ekleme
+		void addPerson(Person person) {
+			// Cache dolu ise en son kullanılan kişiyi sil
+			if (cacheList.size() >= capacity) {
+				Person lastPerson = cacheList.back();
+				cacheList.pop_back();
+				cacheMap.erase(lastPerson.idNumber);
+			}
+
+			// Yeni kişiyi cache'in başına ekle
+			cacheList.push_front(person);
+			cacheMap[person.idNumber] = cacheList.begin();
+		}	
 };
 
 unsigned long long hashString(string str) {
@@ -94,8 +93,7 @@ int findSmallestPrime(int n) {
 	}
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
 	int cacheSize;
 	std::cout << "please input cache size" << std::endl;
 	std::cin >> cacheSize;
@@ -109,8 +107,7 @@ int main(int argc, char const *argv[])
 
 	LruCache cache(cacheSize, loadFactor);
 
-	while (true)
-	{
+	while (true) {
 		int choice;
 		std::cout << endl;
         std::cout << "1. Add Person" << endl;
@@ -121,37 +118,37 @@ int main(int argc, char const *argv[])
 
         if (choice == 1) {
             string idNumber, fullName, birthDate, adress;
-            std::cout << "Kimlik No: ";
+            std::cout << "Id Number: ";
             std::cin >> idNumber;
-            std::cout << "Ad Soyad: ";
+            std::cout << "Fullname ";
             std::cin >> fullName;
-            std::cout << "Doğum Tarihi: ";
+            std::cout << "Birtdate: ";
             std::cin >> birthDate;
-            std::cout << "Adres: ";
+            std::cout << "Adress: ";
             std::cin >> adress;		
 
 
             Person person(idNumber, fullName, birthDate, adress);
             cache.addPerson(person);
-            cout << "Kişi başarıyla eklendi." << endl;
+            std::cout << "Person succesfully added!" << endl;
 		} else if (choice == 2) {
             string idNumber;
-            cout << "Kimlik No: ";
+            std::cout << "Id Number: ";
             cin >> idNumber;
 
             Person* person = cache.getPerson(idNumber);
             if (person != NULL) {
-                cout << "Kimlik No: " << person->idNumber << endl;
-                cout << "Ad Soyad: " << person->fullName << endl;
-                cout << "Doğum Tarihi: " << person->birthDate << endl;
-                cout << "Adres: " << person->adress << endl;
+                std::cout << "Id Number: " << person->idNumber << endl;
+                std::cout << "Fullname: " << person->fullName << endl;
+                std::cout << "Birthdate: " << person->birthDate << endl;
+                std::cout << "Adress: " << person->adress << endl;
             } else {
-                cout << "Kişi bulunamadı." << endl;
+                std::cout << "Person couldnt found." << endl;
             }
         } else if (choice == 3) {
             break;
         } else {
-            cout << "Geçersiz seçim. Tekrar deneyin." << endl;
+            std::cout << "Invalid selection. Try again." << endl;
         }	
 	}
 
